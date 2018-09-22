6261c0d62e3afcedd25c4e38b47ae634dc05965f
Cessario <43488314+Cessario@users.noreply.github.com>
2018-09-22 20:13:38 +0700

    Add files via upload

---
+++ src/pbo2/pkg10117063/latihan2/deklarasi/pengaksesan/PBO210117063Latihan2DeklarasiPengaksesan.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10117063.latihan2.deklarasi.pengaksesan;
+
+/**
+ *
+ * @author DEADPOOL
+ *NAMA : Cessario
+ *NIM : 10117063
+ *Kelas : IF-2
+ *Deskrpsi Program : Menampilkan Deklarasi Pengaksesan */
+public class PBO210117063Latihan2DeklarasiPengaksesan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Deklarasi
+        int nilaiInt;
+        final double PHI = 3.14; //konstanta uppercase
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai ke variabel
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D' ;
+        
+        
+        //Menampilkan Hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai = " + nilaiInt);
+        System.out.println("Isi variabel PHI = " + PHI);
+        System.out.println("Isi variabel Logika = " + nilaiLogika);
+        System.out.println("Isi variabel Karakter = " + nilaiKarakter);
+        
+    }
+    
+}
